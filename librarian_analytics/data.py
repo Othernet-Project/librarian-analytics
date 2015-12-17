207712f1b804ab6137541a4d4080a872f8cc5e1a
Branko Vukelic <branko@outernet.is>
2015-12-17 10:33:38 +0100

    Include OS family in the tracking cookie
    Signed-off-by: Branko Vukelic <branko@outernet.is>

--- librarian_analytics/data.py
+++ librarian_analytics/data.py
@@ -20,13 +20,14 @@ def generate_user_id():
 
 def characterize_agent(ua_string):
     ua = user_agents.parse(ua_string)
+    os_fam = ua.os.family
     if ua.is_pc:
-        return DESKTOP
+        return (os_fam, DESKTOP)
     elif ua.is_tablet:
-        return TABLET
+        return (os_fam, TABLET)
     elif ua.is_mobile:
-        return PHONE
-    return OTHER
+        return (os_fam, PHONE)
+    return (os_fam, OTHER)
 
 
 def serialize_cookie_data(*args):
@@ -35,10 +36,10 @@ def serialize_cookie_data(*args):
 
 def deserialize_cookie_data(data):
     try:
-        user_id, agent_type = data.split(FIELD_SEPARATOR)
+        user_id, os_fam, agent_type = data.split(FIELD_SEPARATOR)
         agent_type = int(agent_type)
         assert agent_type in [DESKTOP, TABLET, PHONE, OTHER], 'Invalid type?'
         assert len(user_id) == 8, 'Invalid user ID?'
-        return user_id, agent_type
+        return user_id, os_fam, agent_type
     except (ValueError, TypeError):
         return None
--- librarian_analytics/plugins.py
+++ librarian_analytics/plugins.py
@@ -31,22 +31,24 @@ def set_track_id(cookie_name):
     cookie_data = request.get_cookie(cookie_name)
     if cookie_data:
         # Cookie is already set, so we don't touch it
-        user_id, agent_type = data.deserialize_cookie_data(cookie_data)
+        user_id, os_fam, agent_type = data.deserialize_cookie_data(cookie_data)
         request.tracking_info = {
             'user_id': user_id,
-            'agent_type': agent_type
+            'os_family': os_fam,
+            'agent_type': agent_type,
         }
         return
 
     # Generate new cookie data
     user_id = data.generate_user_id()
     ua = request.headers.get('User-Agent')
-    agent_type = data.characterize_agent(ua)
+    os_fam, agent_type = data.characterize_agent(ua)
     request.tracking_info = {
         'user_id': user_id,
-        'agent_type': agent_type
+        'os_family': os_fam,
+        'agent_type': agent_type,
     }
-    cookie_data = data.serialize_cookie_data(user_id, agent_type)
+    cookie_data = data.serialize_cookie_data(user_id, os_fam, agent_type)
     response.set_cookie(cookie_name, cookie_data, path='/')
 
 
