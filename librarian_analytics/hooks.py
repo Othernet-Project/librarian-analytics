d9675944ad162977f1cd34bbd7450c1281cdce4f
Andrean Franc <andrean.franc@gmail.com>
2015-12-17 18:01:08 +0100

    Fix task scheduling args

--- librarian_analytics/hooks.py
+++ librarian_analytics/hooks.py
@@ -4,6 +4,6 @@ from .tasks import send_collected_data
 def post_start(supervisor):
     send_interval = supervisor.config['analytics.send_interval']
     supervisor.exts.tasks.schedule(send_collected_data,
-                                   args=(supervisor),
+                                   args=(supervisor,),
                                    delay=send_interval,
                                    periodic=True)
