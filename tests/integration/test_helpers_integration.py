bf5a30ad622ca78a396d09b63ebc94b754a06d2b
Andrean Franc <andrean.franc@gmail.com>
2016-04-20 16:37:57 +0200

    Simplify slice

--- tests/integration/test_helpers_integration.py
+++ tests/integration/test_helpers_integration.py
@@ -102,7 +102,7 @@ def test_cleanup_stats(sorted_database):
     assert rowcount == len(test_data) - max_records
     # make sure the stats found after the cleanup are the stats that were the
     # last ones in the results set acquired before cleanup
-    expected = reversed(stats_before[-1:-(max_records + 1):-1])
+    expected = stats_before[-max_records:]
     assert all([a['id'] == b['id'] for (a, b) in zip(stats_after, expected)])
 
 
