73d8c746404abc4e1bd373be1416f2a7746b586d
Andrean Franc <andrean.franc@gmail.com>
2016-06-08 14:51:10 +0200

    Remove obsolete unittests

--- tests/unit/test_helpers.py
+++ tests/unit/test_helpers.py
@@ -26,39 +26,3 @@ def test_get_stats_bitstream(get_stats, merge_streams):
 def test_get_stats_bitstream_no_stats(get_stats):
     get_stats.return_value = []
     assert mod.get_stats_bitstream(db=mock.Mock(), limit=None) == ([], b'')
-
-
-# GENERAL
-
-
-@pytest.mark.parametrize('max,ret', [
-    (3, [True, True, True, False, False, False]),
-    (4, [True, True, True, True, False, False]),
-    (2, [True, True, False, False, False, False]),
-    (6, [True, True, True, True, True, True]),
-])
-def test_counter(max, ret):
-    c = mod.counter(max)
-    results = []
-    for i in range(6):
-        results.append(c())
-    assert results == ret
-
-
-def test_counter_zero():
-    c = mod.counter(0)
-    assert c() is False
-    assert c() is False
-
-
-@pytest.mark.parametrize('size,out', [
-    (2, [[1, 2], [3, 4], [5, 6], [7, 8], [9, 10]]),
-    (3, [[1, 2, 3], [4, 5, 6], [7, 8, 9], [10]]),
-    (5, [[1, 2, 3, 4, 5], [6, 7, 8, 9, 10]]),
-    (10, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]),
-    (20, [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]),
-    (0, []),
-])
-def test_batch(size, out):
-    source = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    assert list(mod.batches(source, size)) == out
